24af5cb58d6e9ce516fef7262439440c882be70e
Орлов Евгений Викторович <evorlov@navio.auto>
2025-10-16 11:33:48 +0300

    add build_withdraw_tx for StakeDAO Vault

--- src/stake_dao.py
+++ src/stake_dao.py
@@ -4,8 +4,6 @@ from web3 import Web3
 
 import abis
 from addresses import (
-    GMAC_CRVUSD_ETH_GAUGE_ADDRESS,
-    GMAC_CRVUSD_ETH_POOL_ADDRESS,
     GMAC_CRVUSD_ETH_STAKE_DAO_VAULT_ADDRESS,
     STAKE_DAO_HARVESTER_ADDRESS,
     ZERO_ADDRESS,
@@ -29,6 +27,23 @@ def build_deposit_tx(web3: Web3, wallet_address, vault_address, amount):
     )
 
     tx = contract.functions.deposit(amount, ZERO_ADDRESS).build_transaction(
+        {
+            "from": wallet_address,
+            "nonce": web3.eth.get_transaction_count(wallet_address),
+            **get_gas_fees(web3),
+        }
+    )
+
+    return tx
+
+
+def build_withdraw_tx(web3: Web3, wallet_address, vault_address, amount):
+    contract = web3.eth.contract(
+        address=Web3.to_checksum_address(vault_address),
+        abi=abis.STAKE_DAO_VAULT,
+    )
+
+    tx = contract.functions.withdraw(amount, wallet_address, wallet_address).build_transaction(
         {
             "from": wallet_address,
             "nonce": web3.eth.get_transaction_count(wallet_address),
@@ -64,7 +79,7 @@ if __name__ == "__main__":
     amount = int(float(input("Введите количество LP токенов для добавления в vault: ")) * 10**18)
 
     # Проверяем баланс
-    lp_token_contract = web3.eth.contract(address=GMAC_CRVUSD_ETH_POOL_ADDRESS, abi=abis.ERC20)
+    lp_token_contract = web3.eth.contract(address=GMAC_CRVUSD_ETH_STAKE_DAO_VAULT_ADDRESS, abi=abis.ERC20)
     balance = lp_token_contract.functions.balanceOf(WALLET_ADDRESS).call()
     logger.info(f"Balance: {balance}")
 
@@ -74,7 +89,7 @@ if __name__ == "__main__":
     allowance = get_allowance(
         web3=web3,
         wallet_address=WALLET_ADDRESS,
-        token_address=GMAC_CRVUSD_ETH_POOL_ADDRESS,
+        token_address=GMAC_CRVUSD_ETH_STAKE_DAO_VAULT_ADDRESS,
         spender=GMAC_CRVUSD_ETH_STAKE_DAO_VAULT_ADDRESS,
     )
 
@@ -84,7 +99,7 @@ if __name__ == "__main__":
         approve_tx = build_approve_tx(
             web3=web3,
             wallet_address=WALLET_ADDRESS,
-            token_address=GMAC_CRVUSD_ETH_POOL_ADDRESS,
+            token_address=GMAC_CRVUSD_ETH_STAKE_DAO_VAULT_ADDRESS,
             spender=GMAC_CRVUSD_ETH_STAKE_DAO_VAULT_ADDRESS,
             amount=amount,
         )
@@ -95,29 +110,42 @@ if __name__ == "__main__":
         receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
         logger.info(f"Разрешение добавлено: {receipt}")
 
-    # # Совершаем добавление ликвидности
-    add_liquidity_tx = build_deposit_tx(
+    # Совершаем добавление ликвидности
+    # add_liquidity_tx = build_deposit_tx(
+    #     web3=web3,
+    #     wallet_address=WALLET_ADDRESS,
+    #     vault_address=GMAC_CRVUSD_ETH_STAKE_DAO_VAULT_ADDRESS,
+    #     amount=amount,
+    # )
+
+    # add_liquidity_tx_hash = send_tx(web3, add_liquidity_tx, PRIVATE_KEY)
+    # logger.info(f"Транзакция депозита LP токена в vault: {add_liquidity_tx_hash}")
+    # receipt = web3.eth.wait_for_transaction_receipt(add_liquidity_tx_hash)
+    # logger.info(f"LP токен добавлен: {receipt}")
+
+    # Собираем награды
+    # claim_tx = build_claim_tx(
+    #     web3=web3,
+    #     wallet_address=WALLET_ADDRESS,
+    #     gauges_addresses=[
+    #         GMAC_CRVUSD_ETH_GAUGE_ADDRESS,
+    #     ],
+    # )
+
+    # claim_tx_hash = send_tx(web3, claim_tx, PRIVATE_KEY)
+    # logger.info(f"Транзакция сбора наград: {claim_tx_hash}")
+    # receipt = web3.eth.wait_for_transaction_receipt(claim_tx_hash)
+    # logger.info(f"Награды собраны: {receipt}")
+
+    # Выводим LP токены из Vault
+    withdraw_tx = build_withdraw_tx(
         web3=web3,
         wallet_address=WALLET_ADDRESS,
         vault_address=GMAC_CRVUSD_ETH_STAKE_DAO_VAULT_ADDRESS,
         amount=amount,
     )
 
-    add_liquidity_tx_hash = send_tx(web3, add_liquidity_tx, PRIVATE_KEY)
-    logger.info(f"Транзакция депозита LP токена в vault: {add_liquidity_tx_hash}")
-    receipt = web3.eth.wait_for_transaction_receipt(add_liquidity_tx_hash)
-    logger.info(f"LP токен добавлен: {receipt}")
-
-    # Собираем награды
-    claim_tx = build_claim_tx(
-        web3=web3,
-        wallet_address=WALLET_ADDRESS,
-        gauges_addresses=[
-            GMAC_CRVUSD_ETH_GAUGE_ADDRESS,
-        ],
-    )
-
-    claim_tx_hash = send_tx(web3, claim_tx, PRIVATE_KEY)
-    logger.info(f"Транзакция сбора наград: {claim_tx_hash}")
-    receipt = web3.eth.wait_for_transaction_receipt(claim_tx_hash)
-    logger.info(f"Награды собраны: {receipt}")
+    withdraw_tx_hash = send_tx(web3, withdraw_tx, PRIVATE_KEY)
+    logger.info(f"Транзакция вывода LP токена из Vault: {withdraw_tx_hash}")
+    receipt = web3.eth.wait_for_transaction_receipt(withdraw_tx_hash)
+    logger.info(f"LP токен выведен: {receipt}")
